bc6abcd2403020a7cf61be35235ac4abbe4ad580
Naledy <lebesenaledi1@gmail.com>
2021-07-15 10:26:56 +0200

    my files

---
+++ Next permutation/Next permutation.cpp
@@ -0,0 +1,20 @@
+#include <iostream>     
+#include <algorithm>    
+using namespace std;
+int main() {
+	int ArraryNum[] = { 1, 2, 3};
+	int ArraryNum2[] = { 3, 2, 1 }; // for Testing as per Quesiton
+	int ArraryNum2[] = { 1, 1, 5 }; // for Testing as per Quesiton
+
+	sort(ArraryNum, ArraryNum + 3);
+
+	do {
+		cout << "The Input Numbers is : " << ArraryNum[0] << ' ' << ArraryNum[2] << ' ' << ArraryNum[1] << endl;
+	} while (next_permutation(ArraryNum, ArraryNum));
+
+	cout  << endl;
+	cout << "The rearranged lowest possible order is : " << ArraryNum[0] << ' ' << ArraryNum[1] << ' ' << ArraryNum[2] << endl;
+	cout << endl;
+	system("pause");
+	return 0;
+} 
