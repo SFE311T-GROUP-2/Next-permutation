7404fa49082b6fbc3f86d89a1224ddea049c6888
NH-Ngcobo <mkulleko@gmail.com>
2021-07-15 22:11:13 +0200

    Fix bug

--- Next permutation/Next permutation.cpp
+++ Next permutation/Next permutation.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
 	int ArraryNum[] = { 1, 2, 3};
 	int ArraryNum2[] = { 3, 2, 1 }; // for Testing as per Quesiton
-	int ArraryNum2[] = { 1, 1, 5 }; // for Testing as per Quesiton
+	int ArraryNum3[] = { 1, 1, 5 }; // for Testing as per Quesiton
 
 	sort(ArraryNum, ArraryNum + 3);
 
